7636272f188fb9899d76c035176b6a5a4a56e842
holmes.liang <holmes.liang@bytesforce.com>
2022-06-08 11:11:35 +0800

    配置

--- src/desk/ui/page/admin/user/index.tsx
+++ src/desk/ui/page/admin/user/index.tsx
@@ -1,17 +1,5 @@
-import React from "react";
-import { PButton } from "desk/ui/component/antd/button";
-import { MyStorage } from "desk/storage";
-import { useHistory } from "react-router";
-import { LoginPath } from "desk/consts/path/login";
-
 const UserInfo = () => {
-  const history = useHistory();
-
-  return (
-    <>
-      user info
-    </>
-  );
+  return <>user info</>;
 };
 
 export default UserInfo;
